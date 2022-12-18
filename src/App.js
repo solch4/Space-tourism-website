38f6cc813579720b5866cf0d6a3455a4fc0ba634
solch4 <sol.maldonado.0209@gmail.com>
2022-12-18 17:49:59 -0300

    home finished

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
-import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <BrowserRouter>
-      <NavBar />
       <Routes>
         <Route path='/' element={<Home />} />
       </Routes>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,35 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import NavBar from "./NavBar";
 
 function Home() {
   return (
-    <div>Hello world</div>
+    <div className="body-home">
+      <NavBar />
+      <div className="grid-container grid-container--home">
+        <div>
+          <h1 className="uppercase fs-500 ff-sans-cond letter-spacing-1">
+            So, you want to travel to
+            <br />
+            <span className="fs-900 ff-serif">space</span>
+          </h1>
+          <p>
+            Let’s face it; if you want to go to space, you might as well
+            genuinely go to outer space and not hover kind of on the edge of it.
+            Well sit back, and relax because we’ll give you a truly out of this
+            world experience!
+          </p>
+        </div>
+        <div>
+          <Link
+            to="/destination"
+            className="home-btn uppercase fs-600 ff-serif bg-white text-dark"
+          >
+            Explore
+          </Link>
+        </div>
+      </div>
+    </div>
   );
 }
 
