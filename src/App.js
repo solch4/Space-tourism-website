7cdfce99485868788c8f1a998c2b2dbe5d4ff293
solch4 <sol.maldonado.0209@gmail.com>
2022-12-22 01:05:32 -0300

    destination finished

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
+import Destination from "./components/Destination";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/destination' element={<Destination />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/Destination.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import NavBar from "./NavBar";
+import data from "../data/data.json";
+const { destinations } = data;
+
+function Destination() {
+  const [selectedPlanet, setSelectedPlanet] = useState({
+    name: destinations[0].name,
+    images: {
+      png: destinations[0].images.png,
+      webp: destinations[0].images.webp,
+    },
+    description: destinations[0].description,
+    distance: destinations[0].distance,
+    travel: destinations[0].travel,
+  });
+
+  const handleClick = (e) => {
+    const planet = destinations.find((d) => d.name === e.target.id);
+    // console.log('planet',planet);
+    setSelectedPlanet(planet);
+  };
+
+  return (
+    <div className="body-destination">
+      <NavBar />
+      <main className="grid-container grid-container--destination">
+        <h1 className="numbered-title uppercase fs-500 ff-sans-cond letter-spacing-1">
+          <span aria-hidden="true">01</span>Pick your destination
+        </h1>
+        <img src={selectedPlanet.images.png} alt={selectedPlanet.name} />
+        <ul className="tab-list flex fs-200 letter-spacing-2 ff-sans-cond uppercase">
+          {destinations.map((d) => (
+            <li className={selectedPlanet.name === d.name ? "active" : null} onClick={handleClick} id={d.name} key={d.name}>
+              {d.name}
+            </li>
+          ))}
+        </ul>
+        <div className="destination-info">
+          <h2 className="uppercase fs-800 ff-serif">{selectedPlanet.name}</h2>
+          <p className="text-accent">{selectedPlanet.description}</p>
+          <div className="destination-extra-info flex uppercase">
+            <div>
+              <h3 className="fs-200 text-accent letter-spacing-3">Avg. distance</h3>
+              <p className="ff-serif">{selectedPlanet.distance}</p>
+            </div>
+            <div>
+              <h3 className="fs-200 text-accent letter-spacing-3">Est. travel time</h3>
+              <p className="ff-serif">{selectedPlanet.travel}</p>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default Destination;
